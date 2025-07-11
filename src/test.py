bec7d0dc77286e04a880d2ca95bdb05eb8a9dede
Krrish <krrishk0406@gmail.com>
2025-07-11 14:20:57 +0530

    Remove talk.md from repository and add to .gitignore

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyC32Crt1UatpRmXlmV2Xgz7Npp-eYfYH2A")
-model = genai.GenerativeModel("gemini-1.5-pro")
-try:
-    response = model.generate_content("Hello, gemini!")
-    print(response)
-except Exception as e:
-    print("Error:", e)
